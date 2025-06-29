a4fa44e46e84631b0ebd08c8dcaefd07568716ae
Shubham Waghmare <gamermask64@gmail.com>
2025-06-29 19:26:07 +0530

    Testing yolo with Webcam

--- Yolo With Webcam/yolo-webcam.py
+++ Yolo With Webcam/yolo-webcam.py
@@ -4,14 +4,14 @@ from ultralytics import YOLO
 
 cap = cv2.VideoCapture(0)
 model = YOLO("../Yolo-Weights/yolov8n.pt")
-cap.set(3,640)
-cap.set(4,480)
+cap.set(3,1920)
+cap.set(4,1080)
 
 while True:
     success, img = cap.read()
+    
     if not success:
         break
-
 
     results = model(img, stream=True)
 
@@ -20,14 +20,13 @@ while True:
         boxes = r.boxes
         for box in boxes:
             # Get coordinates
-            x1, y1, x2, y2 = map(int, box.xyxy[0])  # Bounding box
+            x1, y1, w, h = map(int, box.xywh[0])  # Bounding box
             conf = float(box.conf[0])               # Confidence
             cls = int(box.cls[0])                   # Class ID
-
+            
+            bbox = x1,y1,w,h
             # Draw rectangle and label
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
-            cv2.putText(img, f'{model.names[cls]} {conf:.2f}', (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 255), 2)
+            cvzone.cornerRect(img,bbox)
 
     # Show image
     cv2.imshow("Image", img)
