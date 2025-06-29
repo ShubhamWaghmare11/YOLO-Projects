ddfbb781453b1fa7e31f2d0c361f201ccd1e9d23
Shubham Waghmare <gamermask64@gmail.com>
2025-06-29 19:54:28 +0530

    added Conf and Class values

--- Yolo With Webcam/yolo-webcam.py
+++ Yolo With Webcam/yolo-webcam.py
@@ -4,8 +4,11 @@ from ultralytics import YOLO
 
 cap = cv2.VideoCapture(0)
 model = YOLO("../Yolo-Weights/yolov8n.pt")
-cap.set(3,1920)
-cap.set(4,1080)
+cap.set(3,1280)
+cap.set(4,720)
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
 
 while True:
     success, img = cap.read()
@@ -20,13 +23,17 @@ while True:
         boxes = r.boxes
         for box in boxes:
             # Get coordinates
-            x1, y1, w, h = map(int, box.xywh[0])  # Bounding box
-            conf = float(box.conf[0])               # Confidence
-            cls = int(box.cls[0])                   # Class ID
+            x1, y1, x2, y2 = map(int, box.xyxy[0])  # Bounding box
+            conf = round(float(box.conf[0]),2)               # Confidence
+            cls = box.cls[0]                   # Class 
             
-            bbox = x1,y1,w,h
+            w, h = x2-x1, y2-y1
+            
             # Draw rectangle and label
-            cvzone.cornerRect(img,bbox)
+            cvzone.cornerRect(img,(x1,y1,w,h))
+            cvzone.putTextRect(img,f"{classNames[int(cls)]} {conf}",(max(0,x1),max(35,y1-20)), scale=1,thickness=1)
+
+            
 
     # Show image
     cv2.imshow("Image", img)
