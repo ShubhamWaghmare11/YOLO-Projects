2afab2d3edb27edcf71bd99355105cecd6374480
maskgamer <gamermask64@gmail.com>
2025-06-30 20:36:12 +0530

    added Total Counter

--- Project 1- Car Counter/car_counter.py
+++ Project 1- Car Counter/car_counter.py
@@ -17,9 +17,12 @@ mask = cv2.resize(mask, (1280,720))
 
 tracker = Sort(max_age=20,min_hits=3, iou_threshold=0.3)
 
+limits = [400,297,673,297]
+
 if len(mask.shape) == 2 or mask.shape[2] == 1:
     mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
 
+totalCount = set()
 
 while True:
     success, img = cap.read()
@@ -48,20 +51,28 @@ while True:
             if currentclass in ['car','truck','bus','motorbike'] and conf > 0.3:
                 # Draw rectangle and label
 
-                cvzone.cornerRect(img,(x1,y1,w,h), l=9, rt = 5)
-                cvzone.putTextRect(img,f"{currentclass} {conf}",(max(0,x1),max(35,y1-20)), scale=1,thickness=1, offset=5)
+                # cvzone.cornerRect(img,(x1,y1,w,h), l=9, rt = 5)
+                # cvzone.putTextRect(img,f"{currentclass} {conf}",(max(0,x1),max(35,y1-20)), scale=1,thickness=1, offset=5)
                 currentArray = np.array([x1, y1, x2, y2, conf])
                 detections = np.vstack((detections,currentArray))
 
-    resultsTracker = tracker.update(detections)
+                resultsTracker = tracker.update(detections)
+                cv2.line(img, (limits[0],limits[1]),(limits[2],limits[3]),(0,0,255),5)
+                for result in resultsTracker:
+                    x1, y1, x2, y2, Id = map(int,result)
+                    w, h = x2-x1, y2-y1
 
-    for result in resultsTracker:
-        x1, y1, x2, y2, Id = result
-        w, h = x2-x1, y2-y1
+                    print(result)
+                    cvzone.cornerRect(img,(x1,y1,w,h),l=9, rt=2, colorR=(255,0,0))
+                    cvzone.putTextRect(img, f"{currentclass} {Id}", (max(0,x1), max(35,y1)),  scale=2,thickness=5, offset=5)
 
-        print(result)
-        cvzone.cornerRect(img,(x1,y1,w,h),l=9, rt=2, colorR=(255,0,0))
-        cvzone.putTextRect(img, f"{currentclass} {Id}", (max(0,x1), max(35,y1)))
+                    cx, cy = x1 + w//2,y1 + h//2
+                    cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
+
+                    if limits[0]< cx < limits[2] and limits[1] -20 < cy < limits[1] + 20:
+                        totalCount.add(Id)
+                    cvzone.putTextRect(img, f"Count: {len(totalCount)}", (50,50),  scale=2,thickness=5, offset=5)
+
 
     # Show image
     cv2.imshow("Image", img)
